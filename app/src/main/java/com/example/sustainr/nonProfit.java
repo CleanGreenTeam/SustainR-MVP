5c1196efade302923120fa398395c1122edacc11
Chloe Hebert <chhebert@calpoly.edu>
2019-01-19 07:15:35 -0800

    added volunteer, nonprofit files

---
+++ app/src/main/java/com/example/sustainr/nonProfit.java
@@ -0,0 +1,13 @@
+package com.example.sustainr;
+
+public class nonProfit {
+
+    private String name;
+    private String location;
+
+    public nonProfit(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+}
